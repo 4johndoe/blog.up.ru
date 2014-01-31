4576f7ffb741f6dfe002b38e0cb2a52327b429ca
John Doe <mailbox4johndoe@gmail.com>
2014-01-31 21:02:51 +0200

    goooooooooo

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <title>my.blog</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Index PAGE</h1>
+<p>
+    <?php
+    print 2*32
+    ?>
+</p>
+</body>
+</html>
